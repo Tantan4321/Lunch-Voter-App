08fe8892fe002e089eb78bf9215a2337cb2385a7
Archit Das <archit.das@outlook.com>
2020-02-04 11:21:04 -0600

    Add screen to see database

--- lib/other_screen.dart
+++ lib/other_screen.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
 
+import 'Database.dart';
+import 'LunchModel.dart';
+
 class SecondPage extends StatefulWidget {
   SecondPage({Key key, this.title}) : super(key: key);
 
@@ -15,21 +18,40 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: Text("Flutter SQLite")),
+      body: FutureBuilder<List<Lunch>>(
+        future: DBProvider.db.getAllClients(),
+        builder: (BuildContext context, AsyncSnapshot<List<Lunch>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                Lunch item = snapshot.data[index];
+                return ListTile(
+                  title: Text(item.lastName),
+                  leading: Text(item.id.toString()),
+                  trailing: Checkbox(
+                    onChanged: (bool value) {
+                      DBProvider.db.blockClient(item);
+                      setState(() {});
+                    },
+                    value: item.blocked,
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text(
-                'SECOND PAGE!',
-                style: TextStyle(fontSize: 40.0, color: Colors.green),
-              ),
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          Client rnd = testClients[math.Random().nextInt(testClients.length)];
+          await DBProvider.db.newClient(rnd);
+          setState(() {});
+        },
       ),
     );
   }
